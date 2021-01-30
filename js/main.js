377c711afb08c53b972e0acec4281efb9d1cfb74
SUNGBEOMCHOI <37692743+SUNGBEOMCHOI@users.noreply.github.com>
2021-01-31 08:06:16 +0900

    Add experience section
    Add experience section

--- js/main.js
+++ js/main.js
@@ -62,6 +62,69 @@ $(document).ready(function(){
   
 });
 
+
+//Experience 슬라이드 부분
+function experience_slide(){
+  var sliderWrapper = document.getElementsByClassName('experience-container'), //클래스명 experience-container
+  sliderContainer = document.getElementsByClassName('experience-slider-container'), //클래스명 experience-container
+  slides = document.getElementsByClassName('experience-slide'), //클래스명 experience-slide
+  slidecount = slides.length, //슬라이드의 개수
+  currentIndex = 0, // 현재보고있는 페이지
+  navPrev = document.getElementById('experience_prev'), // 아이디 experience_prev
+  navNext = document.getElementById('experience_next');  // 아이디 experience_next
+
+
+//슬라이드가 있으면 가로로 배열하기
+for(var i = 0;i<slidecount;i++){
+  slides[i].style.left = i*100 + '%'
+}
+
+// 슬라이드 이동 함수
+function goToSlide(idx){
+  if(idx==0){
+    sliderContainer[0].style.left = 0 + '%';
+  }else{
+    sliderContainer[0].style.left = (idx * -100 ) + '%';
+  }
+  sliderContainer[0].classList.add('animated');
+  currentIndex = idx;
+
+  updateNav();
+}
+
+// 버튼기능 업데이트 함수
+function updateNav(){
+  // 처음일때
+  if(currentIndex == 0){
+    navPrev.classList.add('disabled');
+  }else{
+    navPrev.classList.remove('disabled');
+  }
+  // 마지막일때
+  if(currentIndex == slidecount-1){
+    navNext.classList.add('disabled');
+  }else{
+    navNext.classList.remove('disabled');
+  }
+}
+
+// 버튼을 클릭하면 슬라이드 이동시키기
+navPrev.addEventListener('click', function(){
+  goToSlide(currentIndex - 1);  
+});
+
+navNext.addEventListener('click', function(){
+  goToSlide(currentIndex + 1);
+});
+
+goToSlide(0); // 첫 페이지에서 prev 버튼을 없애주기 위함
+}
+experience_slide()
+
+
+//---------------------------------------------//
+
+function blog_slide(){
 // Blog 슬라이드 부분
 var sliderWrapper = document.getElementsByClassName('container'), //클래스명 container
   sliderContainer = document.getElementsByClassName('slider-container'), //클래스명 slider-container
@@ -116,4 +179,6 @@ navNext.addEventListener('click', function(){
 });
 
 goToSlide(0); // 첫 페이지에서 prev 버튼을 없애주기 위함
+}
 
+blog_slide();
